4422de4a876b76f2fa7da0afc0c682c97cf26301
Beliakov Artem <belyakov.arteom2015@gmail.com>
2021-03-14 19:03:49 +0300

    bug fixed

--- deminf_data/load_data.py
+++ deminf_data/load_data.py
@@ -22,7 +22,6 @@ def load_module(dirname, filename, base_dir="."):
     save_dir = os.path.abspath(".")
 
     base_dir = os.path.abspath(base_dir)
-    os.chdir(os.path.abspath(dirname))
     sys.path.append(base_dir)
     sys.path.append(os.path.join(base_dir, dirname))
 
